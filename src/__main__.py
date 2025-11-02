5017a8611406c84377918ca16e651b7ab13ed2fd
ThatCuteOne <105042017+ThatCuteOne@users.noreply.github.com>
2025-11-02 15:32:27 +0100

    add terry message reactions

--- src/__main__.py
+++ src/__main__.py
@@ -1,20 +1,48 @@
+import asyncio
 import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
-import slash_commands.quote as quote_command
+import logging
+
+logger = logging.getLogger("Terry Bot")
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s] [%(name)s/%(levelname)s] %(message)s',datefmt='%H:%M:%S')
 
 load_dotenv()
 
 token = os.getenv('TOKEN')
 intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(intents=intents)
 
 
-@bot.event
+cogs = ['cogs.quote','cogs.messages']
+
+def register_cogs():
+    logger = logging.getLogger("Cog Registration")
+    done = 0
+    for cog in cogs:
+        try:
+            bot.load_extension(cog)
+            logger.info(f"✓ {cog} loaded successfully")
+            done += 1
+        except Exception as e:
+            logger.exception(f"✗ Failed to load {cog}: {e}")
+    logger.info(f"Sucsessfully registered cogs: {done}/{len(cogs)}")
+
+async def setup_hook():
+    register_cogs()
+
+@bot.listen
 async def on_ready():
-    print("Logged in as a bot {0.user}".format(bot))
+    logger.info("Logged in as a bot {0.user}".format(bot))
+    logger.info(f"Commands registered: {[cmd.name for cmd in bot.application_commands]}")
+    logger.info(f"All cogs loaded: {list(bot.cogs.keys())}")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Eating Universes >:3"))
 
-quote_command.setup(bot)
+def main():
+    asyncio.run(setup_hook())
+    bot.run(token)
 
-bot.run(token)
+
+main()
